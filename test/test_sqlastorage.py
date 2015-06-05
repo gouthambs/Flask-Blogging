25b92ba27fa04ef5a4461e1b27afc3cece129aa9
Gouthaman Balaraman <gouthambs@gmail.com>
2015-06-05 10:23:31 -0700

    Fix tests

--- test/test_sqlastorage.py
+++ test/test_sqlastorage.py
@@ -12,22 +12,22 @@ class TestSQLiteStorage(FlaskBloggingTestCase):
     def _create_storage(self):
         temp_dir = tempfile.gettempdir()
         self._dbfile = os.path.join(temp_dir, "temp.db")
-        engine = create_engine('sqlite:///'+self._dbfile)
-        self.storage = SQLAStorage(engine)
+        self._engine = create_engine('sqlite:///'+self._dbfile)
+        self.storage = SQLAStorage(self._engine)
 
     def setUp(self):
         FlaskBloggingTestCase.setUp(self)
-        self._create_storage(self._db)
+        self._create_storage()
 
     def tearDown(self):
         os.remove(self._dbfile)
 
     def test_post_table_exists(self):
         table_name = "post"
-        with self._db.engine.begin() as conn:
+        with self._engine.begin() as conn:
             self.assertTrue(conn.dialect.has_table(conn, table_name))
             metadata = sqla.MetaData()
-            metadata.reflect(bind=self._db.engine)
+            metadata.reflect(bind=self._engine)
             table = metadata.tables[table_name]
             columns = [t.name for t in table.columns]
             expected_columns = ['id', 'title', 'text', 'post_date', 'last_modified_date', 'draft']
@@ -35,10 +35,10 @@ class TestSQLiteStorage(FlaskBloggingTestCase):
 
     def test_tag_table_exists(self):
         table_name = "tag"
-        with self._db.engine.begin() as conn:
+        with self._engine.begin() as conn:
             self.assertTrue(conn.dialect.has_table(conn, table_name))
             metadata = sqla.MetaData()
-            metadata.reflect(bind=self._db.engine)
+            metadata.reflect(bind=self._engine)
             table = metadata.tables[table_name]
             columns = [t.name for t in table.columns]
             expected_columns = ['id', 'text']
@@ -46,10 +46,10 @@ class TestSQLiteStorage(FlaskBloggingTestCase):
 
     def test_tag_post_table_exists(self):
         table_name = "tag_posts"
-        with self._db.engine.begin() as conn:
+        with self._engine.begin() as conn:
             self.assertTrue(conn.dialect.has_table(conn, table_name))
             metadata = sqla.MetaData()
-            metadata.reflect(bind=self._db.engine)
+            metadata.reflect(bind=self._engine)
             table = metadata.tables[table_name]
             columns = [t.name for t in table.columns]
             expected_columns = ['tag_id', 'post_id']
@@ -57,10 +57,10 @@ class TestSQLiteStorage(FlaskBloggingTestCase):
 
     def test_user_post_table_exists(self):
         table_name = "user_posts"
-        with self._db.engine.begin() as conn:
+        with self._engine.begin() as conn:
             self.assertTrue(conn.dialect.has_table(conn, table_name))
             metadata = sqla.MetaData()
-            metadata.reflect(bind=self._db.engine)
+            metadata.reflect(bind=self._engine)
             table = metadata.tables[table_name]
             columns = [t.name for t in table.columns]
             expected_columns = ['user_id', 'post_id']
@@ -69,7 +69,7 @@ class TestSQLiteStorage(FlaskBloggingTestCase):
     def test_user_post_table_consistency(self):
         # check if the user post table updates the user_id
         user_id = 1; post_id = 5
-        with self._db.engine.begin() as conn:
+        with self._engine.begin() as conn:
             self.storage._save_user_post(user_id, post_id, conn)
             statement = sqla.select([self.storage._user_posts_table])
             result = conn.execute(statement).fetchall()
@@ -86,20 +86,20 @@ class TestSQLiteStorage(FlaskBloggingTestCase):
     def test_tags_uniqueness(self):
         table_name = "tag"
         metadata = sqla.MetaData()
-        metadata.reflect(bind=self._db.engine)
+        metadata.reflect(bind=self._engine)
         table = metadata.tables[table_name]
-        with self._db.engine.begin() as conn:
+        with self._engine.begin() as conn:
             statement = table.insert().values(text="test_tag")
             conn.execute(statement)
         # reentering same tag should raise exception
-        with self._db.engine.begin() as conn:
+        with self._engine.begin() as conn:
             statement = table.insert().values(text="test_tag")
             self.assertRaises(sqla.exc.IntegrityError, conn.execute, statement)
 
     def test_tags_consistency(self):
         tags = ["hello", "world"]
         post_id = 2
-        with self._db.engine.begin() as conn:
+        with self._engine.begin() as conn:
             self.storage._save_tags(tags, post_id, conn)
             statement = sqla.select([self.storage._tag_posts_table]).\
                 where(self.storage._tag_posts_table.c.post_id==post_id)
@@ -116,26 +116,26 @@ class TestSQLiteStorage(FlaskBloggingTestCase):
     def test_tag_post_uniqueness(self):
         table_name = "tag_posts"
         metadata = sqla.MetaData()
-        metadata.reflect(bind=self._db.engine)
+        metadata.reflect(bind=self._engine)
         table = metadata.tables[table_name]
-        with self._db.engine.begin() as conn:
+        with self._engine.begin() as conn:
             statement = table.insert().values(tag_id=1, post_id=1)
             conn.execute(statement)
         # reentering same tag should raise exception
-        with self._db.engine.begin() as conn:
+        with self._engine.begin() as conn:
             statement = table.insert().values(tag_id=1, post_id=1)
             self.assertRaises(sqla.exc.IntegrityError, conn.execute, statement)
 
     def test_user_post_uniqueness(self):
         table_name = "user_posts"
         metadata = sqla.MetaData()
-        metadata.reflect(bind=self._db.engine)
+        metadata.reflect(bind=self._engine)
         table = metadata.tables[table_name]
-        with self._db.engine.begin() as conn:
+        with self._engine.begin() as conn:
             statement = table.insert().values(user_id=1, post_id=1)
             conn.execute(statement)
         # reentering same tag should raise exception
-        with self._db.engine.begin() as conn:
+        with self._engine.begin() as conn:
             statement = table.insert().values(user_id=1, post_id=1)
             self.assertRaises(sqla.exc.IntegrityError, conn.execute, statement)
 
