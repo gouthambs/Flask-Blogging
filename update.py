74097fa9bbea653c977c8ea776435409a036df99
kirk <ekirk0@gmail.com>
2016-07-05 13:04:19 -0700

    removed

--- update.py
+++
@@ -1,6 +0,0 @@
-# http://stackoverflow.com/questions/2720014/upgrading-all-packages-with-pip
-import pip
-from subprocess import call
-
-for dist in pip.get_installed_distributions():
-    call("pip install --upgrade " + dist.project_name, shell=True)
